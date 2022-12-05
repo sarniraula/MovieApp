3a4b667d9005d96939f103b237c41041d6298273
Saroj Niraula <sar.niraula162@gmail.com>
2022-12-05 11:02:46 +0545

    Upcoming movie images Sytles and screen dimensions

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import { Text, View } from 'react-native';
 import Home from './screens/Home';
 
 const HelloWorldApp = () => {
-  
-
- 
   return (
     <View
       style={{
--- screens/Home.js
+++ screens/Home.js
@@ -1,32 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { getPopularMovies } from '../services/services';
-import { Text, View } from 'react-native';
+import { getPopularMovies, getUpcomingMovies } from '../services/services';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { SliderBox } from 'react-native-image-slider-box';
 
-
+const dimensions = Dimensions.get('screen');
 const Home = () => {
-    const [movie, setMovie] = useState('')
+    const [movieImages, setMovieImages] = useState([])
     const [error, setError] = useState(false)
 
     useEffect(() => {
-        getPopularMovies().then( movies => {
-        setMovie(movies[0]);
+        getUpcomingMovies().then( movies => {
+            const movieImagesArray = [];
+            movies.forEach( movie => {
+                movieImagesArray.push('https://image.tmdb.org/t/p/w500'+movie.poster_path)
+            });
+            setMovieImages(movieImagesArray);
         }).catch(err => {
-        setError(err);
-        });
+            setError(err);
+        })
+
+        // getPopularMovies().then( movies => {
+        // // setMovie(movies[0]);
+        // }).catch(err => {
+        // setError(err);
+        // });
     }, []);
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Text>Movie Name: {movie.original_title}</Text>
+        <View style={styles.sliderContainer}>
+            <SliderBox 
+                images={movieImages}
+                dotStyle={styles.dotStyle} 
+                autoplay={true} 
+                circleLoop={true}
+                sliderBoxHeight= { dimensions.height / 1.5 } //One third of height of screen
+            />
+            {/* <Text>Movie Name: {movie.original_title}</Text>
             <Text>Language: {movie.original_language}</Text>
-            <Text>Release Date: {movie.release_date}</Text>
+            <Text>Release Date: {movie.release_date}</Text> */}
             {error && <Text style={{color: 'red'}}>Error in the Server</Text>}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    sliderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    dotStyle: {
+        height: 0
+    }
+});
 
 export default Home;
